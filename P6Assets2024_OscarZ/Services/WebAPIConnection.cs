0e461d834eeeb46a13f3da434863e7db45be94de
Oscar <ozumbado2000@gmail.com>
2024-03-21 21:14:04 -0600

    Semana 9

---
+++ P6Assets2024_OscarZ/Services/WebAPIConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P6Assets2024_OscarZ.Services
+{
+    class WebAPIConnection
+    {
+        //aca definimos ta ruta raiz del URL det servicio web (API Rest)
+        // a esta raiz, que Itamaremos Prefijo, se Ie debe adjuntar
+        //el resto de ta ruta URL, que llamares Sufijo, para cada
+        //endpoint que vayamos a usar
+
+        //ademas es im ortante indicar ue normalmente los APIs
+        //trabajan en dos versiones: Pruebas y Produccion
+
+        public static string ProductionURLPrefix = "http://192.168.100.2:45455/api/";
+
+        public static string TestingURLPrefix = "http://192.168.100.2:45455/api/";
+
+        //ademas de la raiz de URL aca vamos a indicar cual es la API KEY que deberia
+        // usar para consumir los endpoints
+
+        public static string ApiKeyName = "P6ApiKey";
+        public static string ApiKeyValue = "OscarDP6Api";
+    }
+}
